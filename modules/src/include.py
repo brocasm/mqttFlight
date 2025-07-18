ae41e294ea2e6de482f223ea8537328eda272cb7
brocsam <brocasm@resk-u.ch>
2025-07-18 23:32:52 +0000

    add filename to log

--- modules/src/include.py
+++ modules/src/include.py
@@ -1,5 +1,8 @@
+import os 
+
 def log(*args, client=None, module_id=None, publish=True):
     message = ' '.join(map(str, args))
     print(message)
     if client and module_id:
-        client.publish(f"cockpit/{module_id}/logs/boot.py", message)
+        filename = os.path.basename(__file__)
+        client.publish(f"cockpit/{module_id}/logs/{filename}", message)
